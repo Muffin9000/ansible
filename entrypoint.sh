353d5839d4313936b9ac2ab46b1db935fe7038cb
Muffin9000 <mymail@mail.com>
2022-09-07 23:49:54 +0300

    add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ansible-galaxy install -r roles/requirements.yml
+echo $ANSIBLE_VAULT_PASSWORD >> .vault
+ansible-playbook -i <dynamicInventory>.yml site.yml --vault-password-file .vault
+#avoids locally storing on a mounted volume
+rm .vault
